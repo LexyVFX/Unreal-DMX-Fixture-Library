312f2735390f8f1c2824eb443d34ff15c201beed
Alexis Abel <dev.jabel@gmail.com>
2020-10-02 19:14:47 -0600

    converted uproject to C++ (necessary for compiling C++ project plugins) by adding placeholder class

---
+++ LexyVfxDmxFixtures/Source/LexyVfxDmxFixtures/CppAPITestObject.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CppAPITestObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEXYVFXDMXFIXTURES_API UCppAPITestObject : public UObject
+{
+	GENERATED_BODY()
+	
+};

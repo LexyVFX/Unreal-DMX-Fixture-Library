94f7c4559c2a48ac06cd73f2afe5b48fd7bd13d5
Alexis Abel <dev.jabel@gmail.com>
2020-10-11 18:43:18 -0600

    Stubbed out a Function Manager Component towards removing the last of the Blueprint code in the C++ FR1 Blueprint asset

---
+++ LexyVfxDmxFixtures/Plugins/LexyVFXCppFixtures/Source/LexyVFXCppFixtures/Private/LexyVFXDMXFunctionManager.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LexyVFXDMXFunctionManager.h"
+
+// Sets default values for this component's properties
+ULexyVFXDMXFunctionManager::ULexyVFXDMXFunctionManager()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void ULexyVFXDMXFunctionManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void ULexyVFXDMXFunctionManager::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
